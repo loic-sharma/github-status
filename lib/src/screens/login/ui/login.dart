c1e82547e53931859d3942c9b3d59db8815a6638
Loic Sharma <sharma.loic@gmail.com>
2024-11-28 14:39:55 -0800

    Update login UI

--- lib/src/screens/login/ui/login.dart
+++ lib/src/screens/login/ui/login.dart
@@ -1,6 +1,7 @@
 import 'package:context_watch/context_watch.dart';
 import 'package:flutter/material.dart' as material;
 import 'package:flutter/widgets.dart';
+import 'package:flutter_octicons/flutter_octicons.dart';
 import 'package:gh_status/main.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
@@ -16,17 +17,18 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     model.watch(context);
 
-
-    return switch (model.state) {
-      StartingState _ => const Text('Starting...'),
-      ErrorState(: var error) => const Text('Error'),
-      CompletedState(: var accessToken) => const Text('Completed'),
-      WaitingState(: final verificationUri, : final userCode) => _Waiting(
-        nextRefreshSeconds: model.nextRefreshSeconds,
-        verificationUri: verificationUri,
-        userCode: userCode,
-      ),
-    };
+    return _LoginLayout(
+      child: switch (model.state) {
+        StartingState _ => const Text('Starting...'),
+        ErrorState(: var error) => const Text('Error'),
+        CompletedState(: var accessToken) => const Text('Completed'),
+        WaitingState(: final verificationUri, : final userCode) => _Waiting(
+          nextRefreshSeconds: model.nextRefreshSeconds,
+          verificationUri: verificationUri,
+          userCode: userCode,
+        ),
+      },
+    );
   }
 }
 
@@ -46,60 +48,77 @@ class _Waiting extends material.StatelessWidget {
   Widget build(material.BuildContext context) {
     final theme = ShadTheme.of(context);
 
-    return SizedBox(
-      width: 480,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Login to GitHub',
-            style: theme.textTheme.h3,
-          ),
-
-          const SizedBox(height: 16),
-
-          RichText(
-            text: TextSpan(
-              children: [
-                const TextSpan(text: 'Open '),
-                TextSpan(
-                  text: verificationUri.toString(),
-                  style: const TextStyle(color: material.Colors.blue),
-                  recognizer: linkRecognizer(url: verificationUri),
-                ),
-                const TextSpan(text: ' and enter:'),
-              ],
-            ),
-          ),
-
-          const SizedBox(height: 10),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [    
+        RichText(
+          text: TextSpan(
             children: [
-              IntrinsicWidth(
-                child: ShadInputFormField(
-                  initialValue: 'ABCD-EFGH',
-                  readOnly: true,
-                ),
+              const TextSpan(text: 'Open '),
+              TextSpan(
+                text: verificationUri.toString(),
+                style: const TextStyle(color: material.Colors.blue),
+                recognizer: linkRecognizer(url: verificationUri),
               ),
-      
-              ShadButton.secondary(
-                onPressed: () {},
-                icon: const Icon(material.Icons.copy, size: 16),
-                child: const Text('Copy'),
-              ),
+              const TextSpan(text: ' and enter:'),
             ],
           ),
+        ),
+    
+        const SizedBox(height: 10),
+    
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IntrinsicWidth(
+              child: ShadInputFormField(
+                initialValue: 'ABCD-EFGH',
+                readOnly: true,
+              ),
+            ),
+    
+            ShadButton.secondary(
+              onPressed: () {},
+              icon: const Icon(material.Icons.copy, size: 16),
+              child: const Text('Copy'),
+            ),
+          ],
+        ),
+    
+        const SizedBox(height: 24),
+    
+        Text(
+          'Refreshing in $nextRefreshSeconds seconds...',
+          style: theme.textTheme.muted,
+        ),
+      ],
+    );
+  }
+}
 
-          const SizedBox(height: 24),
+class _LoginLayout extends StatelessWidget {
+  const _LoginLayout({required this.child});
 
-          Text(
-            'Refreshing in $nextRefreshSeconds seconds...',
-            style: theme.textTheme.muted,
-          ),
-        ],
-      ),
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = ShadTheme.of(context);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const SizedBox(height: 64.0),
+        const Icon(OctIcons.mark_github_16, size: 100.0),
+        const SizedBox(height: 16.0),
+        Text(
+          'Login to GitHub',
+          style: theme.textTheme.h3,
+        ),
+        const SizedBox(height: 16.0),
+
+        child,
+      ],
     );
   }
 }
